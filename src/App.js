0881d073941b85070c961132b53963fb13ef90cd
Sahilsaxena9711 <sahilsaxena9711@gmail.com>
2025-02-02 21:26:01 +0530

    [initial commit]

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Header</header>
+      <div>
+        <p>Body</p>
+      </div>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
